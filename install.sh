2fc26b6a44dd4d9af7ce253ebaff9fc9889171d5
Murat Magomedov <murat.magomedov@runpod.io>
2025-04-14 10:19:02 -0700

    remove sudo

--- install.sh
+++ install.sh
@@ -15,14 +15,14 @@ HOSTNAME2_IP=$5
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 # Install slurm
-sudo apt update
-sudo apt install -y slurm-wlm slurm-client munge locales
+apt update
+apt install -y slurm-wlm slurm-client munge locales
 
 current_hostname=$(hostname)
 if [ "$current_hostname" == "$HOSTNAME1" ]; then
-    echo "$HOSTNAME2_IP $HOSTNAME2" | sudo tee -a /etc/hosts
+    echo "$HOSTNAME2_IP $HOSTNAME2" | tee -a /etc/hosts
 elif [ "$current_hostname" == "$HOSTNAME2" ]; then
-    echo "$HOSTNAME1_IP $HOSTNAME1" | sudo tee -a /etc/hosts
+    echo "$HOSTNAME1_IP $HOSTNAME1" | tee -a /etc/hosts
 else
     echo "Error: current hostname is not $HOSTNAME1 or $HOSTNAME2" >&2 && exit 1
 fi
@@ -34,28 +34,28 @@ fi
 # Create munge user and group
 getent passwd munge
 getent group munge
-sudo groupadd -r munge
-sudo useradd -r -g munge -s /sbin/nologin munge
+groupadd -r munge
+useradd -r -g munge -s /sbin/nologin munge
 # Add slurm user to munge group
-sudo usermod -aG munge slurm
+usermod -aG munge slurm
 
 # Create munge directories and set permissions
 for dir in /var/log/munge /var/lib/munge /etc/munge /var/run/munge /run/munge; do
-    sudo mkdir -p $dir
-    sudo chown -R munge:munge $dir
-    sudo chmod -R 755 $dir
+    mkdir -p $dir
+    chown -R munge:munge $dir
+    chmod -R 755 $dir
 done
 
 echo -n "$MUNGE_KEY_STR" | sha256sum | awk '{print $1}' > /etc/munge/munge.key
-sudo chmod 400 /etc/munge/munge.key
-sudo chown munge:munge /etc/munge/munge.key
+chmod 400 /etc/munge/munge.key
+chown munge:munge /etc/munge/munge.key
 
 # Start munge service
-sudo /usr/sbin/munged --force
+/usr/sbin/munged --force
 
 # Set permissions for munge socket
 if [ ! -f /run/munge/munge.socket.2 ]; then
-    sudo chmod 777 /run/munge/munge.socket.2
+    chmod 777 /run/munge/munge.socket.2
 fi
 
 if munge -n | unmunge; then
@@ -71,17 +71,17 @@ fi
 
 # Create slurm directories and set permissions
 for dir in /etc/slurm /etc/slurm-llnl /var/spool/slurm /var/log/slurm-llnl /var/spool/slurm/ctld /var/lib/slurm-llnl /var/lib/slurm-llnl/slurmctld /var/spool/slurmd /var/lib/slurm-llnl/slurmd /var/log/slurm; do
-    sudo mkdir -p $dir
-    sudo chown -R slurm:slurm $dir
-    sudo chmod -R 755 $dir
+    mkdir -p $dir
+    chown -R slurm:slurm $dir
+    chmod -R 755 $dir
 done
 
 # Create slurm.conf
 bash $SCRIPT_DIR/create_slurm_conf.sh $HOSTNAME1 $HOSTNAME2 $HOSTNAME1_IP $HOSTNAME2_IP > /etc/slurm-llnl/slurm.conf
 bash $SCRIPT_DIR/create_gres_conf.sh $current_hostname > /etc/slurm-llnl/gres.conf
-sudo ln -s /etc/slurm-llnl/slurm.conf /etc/slurm/slurm.conf
-sudo ln -s /etc/slurm-llnl/gres.conf /etc/slurm/gres.conf
+ln -s /etc/slurm-llnl/slurm.conf /etc/slurm/slurm.conf
+ln -s /etc/slurm-llnl/gres.conf /etc/slurm/gres.conf
 
 echo "Now run the following command to start the slurm services:"
-echo "sudo slurmctld -D"
-echo "sudo slurmd -D"
+echo "slurmctld -D"
+echo "slurmd -D"
--- setup.sh
+++ setup.sh
@@ -13,14 +13,5 @@ source .venv/bin/activate
 uv pip install ipykernel simple-gpu-scheduler # very useful on runpod with multi-GPUs
 python -m ipykernel install --user --name=venv # so it shows up in jupyter notebooks within vscode
 
-# 3) Setup dotfiles and ZSH (optional)
-mkdir -p git && cd git
-git clone https://github.com/jplhughes/dotfiles.git
-cd dotfiles
-./install.sh --zsh --tmux
-chsh -s /usr/bin/zsh
-./deploy.sh
-cd ..
-
 # 4) Setup github (optional)
 # echo ./scripts/setup_github.sh "<YOUR_GITHUB_EMAIL>" "<YOUR_NAME>"
